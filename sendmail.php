1c3e5bc6017e9fc9116160f5d2939b6fe5951335
nikitharani <49191249+nikitharani@users.noreply.github.com>
2021-04-06 12:31:33 +0100

    Changes made in sendMail file

--- sendmail.php
+++ sendmail.php
@@ -1,20 +1,7 @@
 <?php
 
-// // dummy details
-// $user_name = 'niki';
-// $user_email = 'nnikitharani21@gmail.com';
-// $user_num = 9099090;
-// $user_compName = 'mucom';
-// $user_business = 'mucom.co.uk';
-// $user_add = '192e';
-// $user_city = 'bir';
-// $user_country = 'BG';
-// $user_zip = 'ni7878';
-// $user_date = '23/07/2021';
-// $user_info = 'information';
-// //  $user_interest = $_POST["person_interest" 
 
-if(isset($_POST["submit"])){
+if(isset($_POST['submit'])){
 $user_name = $_POST["cusName"];
 $user_email = $_POST["cusEmail"];
 $user_num = $_POST["cusNum"];
@@ -26,43 +13,29 @@ $user_country = $_POST["cusCountry"];
 $user_zip = $_POST["cusZip"];
 $user_date = $_POST["inputDate"];
 $user_info = $_POST["inputBrief"];
-//  $user_interest = $_POST["person_interest"];  
 
 
-
-
-$to = "vaijayanti.mahajan@vymahajan.co.uk";
+$to ="rockingkrish5@hotmail.com" ;
+  //"vaijayanti.mahajan@vymahajan.co.uk"
 $subject = "psn page details";
-$message ="Name: ".$user_name. "</br>" . "Email:" .$user_email. "</br>" . "Phone_Num:". $user_num. "</br>" . "Company Name:". $user_compName. "</br>" . 
-"Business Domain:". $user_business. "</br>". "Address:". $user_add. "</br>". "City:". $user_city. "</br>"."Country:". $user_country. "</br>"."Brief:". $user_info;
+$message ="Name: ".$user_name. "\n" . "Email: " .$user_email. "\n" . "Phone_Num: ". $user_num. "\n" . "CompanyName: ". $user_compName. "\n" . 
+"BusinessDomain: ". $user_business. "\n". "Address: ". $user_add. "\n". "City: ". $user_city. "\n"."Country: ". $user_country. "\n"."Brief: ". $user_info;
 $message = wordwrap($message, 70, "\r\n");
 
-$from = $user_email;
-$headers = 'From : '. $from;
 
-// echo $message.'</br>';
-// echo $headers.'</br>';
-// echo $subject.'</br>';
+$headers = "From:".$user_email; 
+    
+  mail($to,$subject,$message,$headers);
+  header("Location:index.html?mailsend");
+   
+  
+  
+  
+  
+}
 
-if(mail($to,$subject,$message, $header)){
-    // echo "<div>This is a primary alert—check it out!</div>";
-    // echo "<div class="alert alert-primary" role="alert">This is a primary alert—check it out!</div>";
-    echo "<h1>Sent Sucessfully! Thank you"." ".$user_name.", We will contact you shortly!</h1>";
-    // echo '<div class="alert alert-primary" role="alert">
-    // <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    // This is a primary alert—check it out!</div>';
-}
-else{
-    echo "Something went wrong!";
-    // echo '<div class="alert alert-primary" role="alert">
-    // <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    // Something went wrong!</div>';
-};
-}
+
+
 
 
 ?>
-
-
-
-
